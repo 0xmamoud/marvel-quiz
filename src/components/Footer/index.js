5916d6a361e00732b81713c0ac8f44a82f25818b
KANE <mahamadoudaba.kane@gmail.com>
2023-06-11 23:25:55 +0200

    added landing page

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <div className='footer-container'>
+            <p>Projet réalisé avec un cours udemy -2023</p>
+            <p>Les icones Wolverine, Iron-man, Spider-man sont prises sur iconFinder.com</p>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
